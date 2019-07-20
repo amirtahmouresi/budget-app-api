fbde3b2106d36b1526c9ede6dfac22baa9e78ab4
Amir Tahmouresi <amir.tahmouresi.1881@gmail.com>
2019-07-20 16:29:19 +0430

    [for test on other os]

--- app/core/admin.py
+++ app/core/admin.py
@@ -9,18 +9,18 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields':('email','password')}),
+        (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
-            {'fields' : ('is_staff','is_active','is_superuser')}
+            {'fields': ('is_staff', 'is_active', 'is_superuser')}
         ),
-        (_('Important Dates'), {'fields' : ('last_login',)})
+        (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
-        (None,{
-            'classes' : ('wide',),
-            'fields' : ('email', 'password1', 'password2')
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
         }),
     )
 
